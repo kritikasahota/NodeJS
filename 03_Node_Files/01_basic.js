d5b11c6c986608e3cae22dd0902636260dd9538c
Kritikasahota <sahotakritika029@gmail.com>
2026-08-17 21:44:06 +0530

    Learned how to create and execute JavaScript files with Node.js

---
+++ 03_Node_Files/01_basic.js
@@ -0,0 +1,39 @@
+// ==========================================
+// NODE.JS FILES - BASIC
+// ==========================================
+
+// Node.js allows us to create and execute JavaScript code using .js files.
+// A JavaScript file can be executed in the terminal using:
+// node filename.js
+
+// Example:
+// node 01_basic.js
+
+
+// ------------------------------------------
+// console.log()
+// ------------------------------------------
+
+// console.log() is used to print information or output to the terminal.
+
+console.log("Hello World");
+
+
+// We can print any text using console.log().
+
+console.log("This is my first Node.js file");
+
+
+// ------------------------------------------
+// Expressions in Node.js
+// ------------------------------------------
+
+// Node.js can execute normal JavaScript expressions and operations.
+
+// Addition
+console.log(10 + 20);
+
+// Multiplication
+console.log(10 * 5);
+
+
