57e03f8025f01c495851e115fe406622b7d572f7
Kritikasahota <sahotakritika029@gmail.com>
2026-08-17 21:46:37 +0530

    Learned exporting multiple values using module.exports

---
+++ 05_Export/02_Multiple_Export/app.js
@@ -0,0 +1,7 @@
+const data = require("./03_multiple_export");
+
+console.log(data.name);
+
+console.log(data.age);
+
+console.log(data.course);
---
+++ 05_Export/02_Multiple_Export/student.js
@@ -0,0 +1,11 @@
+const name = "Kritika";
+
+const age = 20;
+
+const course = "B.Tech IT";
+
+module.exports = {
+    name: name,
+    age: age,
+    course: course
+};
