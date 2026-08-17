2a8eabaa4136353cc38f88e3fbf8bd1a3e196024
Kritikasahota <sahotakritika029@gmail.com>
2026-08-17 21:44:49 +0530

    Learned the Node.js process object

---
+++ 04_Process/01_process.js
@@ -0,0 +1,43 @@
+// ==========================================
+// NODE.JS - PROCESS OBJECT
+// ==========================================
+
+// Node.js provides a built-in global object called 'process'.
+// The process object provides information about and control over the current Node.js process.
+// A Node.js process is the running instance of a Node.js program.
+
+// ------------------------------------------
+// Displaying the Process Object
+// ------------------------------------------
+
+// We can use console.log() to display the process object.
+
+console.log(process);
+
+
+// ------------------------------------------
+// What does process contain?
+// ------------------------------------------
+
+// The process object contains information related to the currently running Node.js process.
+// It provides properties and methods related to:
+// - Node.js version
+// - Operating system
+// - Environment
+// - Current working directory
+// - Command-line arguments
+// - Process execution
+
+
+// ------------------------------------------
+// Important
+// ------------------------------------------
+
+// 'process' is provided by Node.js automatically. We do not need to import or require it.
+
+// Example:
+// console.log(process);
+
+// This will display a large object containing information about the current Node.js process.
+
+
