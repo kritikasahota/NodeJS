b6e1f397b820546ce62126594f119333699c385d
Kritikasahota <sahotakritika029@gmail.com>
2026-08-17 21:45:03 +0530

    Learned process version platform and architecture properties

---
+++ 04_Process/02_process_info.js
@@ -0,0 +1,30 @@
+// ------------------------------------------
+// Node.js Version
+// ------------------------------------------
+
+// process.version returns the version of Node.js that is currently installed and running.
+
+console.log(process.version);
+
+
+// ------------------------------------------
+// Operating System Platform
+// ------------------------------------------
+
+// process.platform returns the operating system platform on which the Node.js process is running.
+// For example:
+// Windows → win32
+
+console.log(process.platform);
+
+// ------------------------------------------
+// System Architecture
+// ------------------------------------------
+
+// process.arch returns the CPU architecture for which the Node.js process is running.
+// For example:
+// x64 → 64-bit architecture
+
+console.log(process.arch);
+
+
