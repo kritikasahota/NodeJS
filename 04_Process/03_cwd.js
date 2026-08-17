dbafe92b9631c599f8db81a8816e5940342e7dd3
Kritikasahota <sahotakritika029@gmail.com>
2026-08-17 21:45:17 +0530

    Learned process.cwd for current working directory

---
+++ 04_Process/03_cwd.js
@@ -0,0 +1,29 @@
+// ==========================================
+// NODE.JS - CURRENT WORKING DIRECTORY
+// ==========================================
+
+// process is a built-in global object provided by Node.js.
+// cwd() stands for "Current Working Directory".
+// process.cwd() returns the path of the directory from which the Node.js process is currently running.
+
+
+// ------------------------------------------
+// Current Working Directory
+// ------------------------------------------
+
+console.log(process.cwd());
+
+
+// ------------------------------------------
+// Example
+// ------------------------------------------
+
+// If you run the file while your terminal is in:
+// F:\NODE JS\04_Process
+
+// process.cwd() will return:
+// F:\NODE JS\04_Process
+
+// The exact output depends on the directory from which you run the Node.js command.
+
+
